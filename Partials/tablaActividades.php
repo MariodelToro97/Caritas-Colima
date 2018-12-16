1a592adf88b825819ee69da79718cc0c00a8b167
Mario Josue del Toro Morales <mario_chomy@outlook.com>
2018-12-16 11:19:34 -0600

    Actualizado de datos Actividades
    Se acomodó el código para que en el actualizado de los datos de la actividad, se generé dentro del modal los que ya tiene, para que de esta forma le sea ma fácil al usuario editar sobre algo que ya tiene.

--- Partials/tablaActividades.php
+++ Partials/tablaActividades.php
@@ -1,6 +1,6 @@
 <div id="tablaActividades" class="table-responsive">
 <!--Inicio de tabla de Actividades-->
-<table class="table table-sm table-striped table-bordered">
+<table id ="EditarTablaModalActividad" class="table table-sm table-striped table-bordered">
   <thead style="background: rgba(240, 47, 47, 0.84);" class="text-white">
     <tr class="text-center" style="font-size: 13px;">
       <!--<th scope="col">ID</th>-->
@@ -20,13 +20,16 @@
 
   <?php
   //$sql = "SELECT * FROM actividades";
-  $sql = "SELECT idActividades, ins.nombreInstitucion as idInstitucion, DATE_FORMAT(fechaActividad,'%d/%m/%Y') AS fechaActividad, asistentesActividad, despensasActividad, actividadExtra, institucionUno, institucionDos, voluntarioActividad, actividadUno, actividadDos
+  $sql = "SELECT idActividades, idInstitucion,  ins.nombreInstitucion as nombreInstitucion, DATE_FORMAT(fechaActividad,'%d/%m/%Y') AS fecha, fechaActividad, asistentesActividad, despensasActividad, actividadExtra, institucionUno, institucionDos, voluntarioActividad, actividadUno, actividadDos
           FROM instituciones as ins INNER JOIN actividades as ac
                 on ac.idInstitucion = ins.idInstituciones
           ORDER BY fechaActividad";
   $result = mysqli_query($conexion, $sql);
 
+  $contador = 0;
+
   while($mostrar=mysqli_fetch_array($result)){
+    $contador++;
 
     if ($mostrar['institucionDos'] == 0) {
       $mostrar['institucionDos'] == '';
@@ -34,9 +37,8 @@
 
     ?>
     <tr class="text-center" style="font-size: 13px; background: rgba(255, 0, 0, 0.44);">
-      <!-- <td><?php //echo $mostrar['idActividades'] ?></td> -->
-      <td><?php echo $mostrar['idInstitucion'] ?></td>
-      <td><?php echo $mostrar['fechaActividad'] ?></td>
+      <td><?php echo $mostrar['nombreInstitucion'] ?></td>
+      <td><?php echo $mostrar['fecha'] ?></td>
       <td><?php echo $mostrar['asistentesActividad'] ?></td>
       <td><?php echo $mostrar['despensasActividad'] ?></td>
       <td><?php echo utf8_encode($mostrar['actividadExtra']) ?></td>
@@ -45,8 +47,10 @@
       <td><?php echo utf8_encode($mostrar['voluntarioActividad']) ?></td>
       <td><?php echo utf8_encode($mostrar['actividadUno']) ?></td>
       <td><?php echo utf8_encode($mostrar['actividadDos']) ?></td>
+      <td hidden><?php echo $mostrar['idInstitucion'] ?></td>
+      <td hidden><?php echo $mostrar['fechaActividad'] ?></td>
       <td>
-        <button class="btn btn-info btn-sm" value="<?php echo $mostrar['idActividades'] ?>" type="button" onclick="acomodarEditarActividades(this)" data-toggle="modal" data-target="#agregarActividad">Editar Campo</button>
+        <button class="btn btn-info btn-sm" value="<?php echo $mostrar['idActividades'] ?>" name="<?php echo $contador ?>" type="button" onclick="acomodarEditarActividades(this)" data-toggle="modal" data-target="#agregarActividad">Editar Campo</button>
         <button class="btn btn-danger btn-sm" value="<?php echo $mostrar['idActividades'] ?>" type="button" name="<?php echo utf8_encode($mostrar['actividadUno']) ?>" data-toggle="modal" data-target="#deleteInstituto" onclick="editarDelete(this)">Eliminar Actividad</button>
       </td>
 
--- assets/js/Actividades.js
+++ assets/js/Actividades.js
@@ -3,47 +3,47 @@ function contador(obj){
   var strLength = obj.value.length;
   var charRemain = (maxLength - strLength);
 
-    if(charRemain == 0){
-        document.getElementById("contadorApoyo").innerHTML = '<span id="contadorApoyo" style="font-size: 12px; float: right;" class="text-danger mt-3 font-weight-bold">Haz llegado al límite de escritura</span>';
-    }else{
-        document.getElementById("contadorApoyo").innerHTML = '<span id="contadorApoyo" style="font-size: 12px; float: right;" class="text-success mt-3 font-weight-bold">'+charRemain+' caracteres restantes</span>';
-    }
+  if(charRemain == 0){
+    document.getElementById("contadorApoyo").innerHTML = '<span id="contadorApoyo" style="font-size: 12px; float: right;" class="text-danger mt-3 font-weight-bold">Haz llegado al límite de escritura</span>';
+  }else{
+    document.getElementById("contadorApoyo").innerHTML = '<span id="contadorApoyo" style="font-size: 12px; float: right;" class="text-success mt-3 font-weight-bold">'+charRemain+' caracteres restantes</span>';
+  }
 };
 
 function contadorDos(obj){
-    var maxLength = 250;
-    var strLength = obj.value.length;
-    var charRemain = (maxLength - strLength);
+  var maxLength = 250;
+  var strLength = obj.value.length;
+  var charRemain = (maxLength - strLength);
 
-    if(charRemain == 0){
-        document.getElementById("contadorvoluntarios").innerHTML = '<span id="contadorvoluntarios" style="font-size: 12px; float: right;" class="text-danger mt-3 font-weight-bold">Haz llegado al límite de escritura</span>';
-    }else{
-        document.getElementById("contadorvoluntarios").innerHTML = '<span id="contadorvoluntarios" style="font-size: 12px; float: right;" class="text-success mt-3 font-weight-bold">'+charRemain+' caracteres restantes</span>';
-    }
+  if(charRemain == 0){
+    document.getElementById("contadorvoluntarios").innerHTML = '<span id="contadorvoluntarios" style="font-size: 12px; float: right;" class="text-danger mt-3 font-weight-bold">Haz llegado al límite de escritura</span>';
+  }else{
+    document.getElementById("contadorvoluntarios").innerHTML = '<span id="contadorvoluntarios" style="font-size: 12px; float: right;" class="text-success mt-3 font-weight-bold">'+charRemain+' caracteres restantes</span>';
+  }
 };
 
 function contadorTres(obj){
-    var maxLength = 1000;
-    var strLength = obj.value.length;
-    var charRemain = (maxLength - strLength);
+  var maxLength = 1000;
+  var strLength = obj.value.length;
+  var charRemain = (maxLength - strLength);
 
-    if(charRemain == 0){
-        document.getElementById("contadorActividadUno").innerHTML = '<span id="contadorActividadUno" style="font-size: 12px; float: right;" class="text-danger mt-3 font-weight-bold">Haz llegado al límite de escritura</span>';
-    }else{
-        document.getElementById("contadorActividadUno").innerHTML = '<span id="contadorActividadUno" style="font-size: 12px; float: right;" class="text-success mt-3 font-weight-bold">'+charRemain+' caracteres restantes</span>';
-    }
+  if(charRemain == 0){
+    document.getElementById("contadorActividadUno").innerHTML = '<span id="contadorActividadUno" style="font-size: 12px; float: right;" class="text-danger mt-3 font-weight-bold">Haz llegado al límite de escritura</span>';
+  }else{
+    document.getElementById("contadorActividadUno").innerHTML = '<span id="contadorActividadUno" style="font-size: 12px; float: right;" class="text-success mt-3 font-weight-bold">'+charRemain+' caracteres restantes</span>';
+  }
 };
 
 function contadorCuatro(obj){
-    var maxLength = 1000;
-    var strLength = obj.value.length;
-    var charRemain = (maxLength - strLength);
+  var maxLength = 1000;
+  var strLength = obj.value.length;
+  var charRemain = (maxLength - strLength);
 
-    if(charRemain == 0){
-        document.getElementById("contadorActividadDos").innerHTML = '<span id="contadorActividadDos" style="font-size: 12px; float: right;" class="text-danger mt-3 font-weight-bold">Haz llegado al límite de escritura</span>';
-    }else{
-        document.getElementById("contadorActividadDos").innerHTML = '<span id="contadorActividadDos" style="font-size: 12px; float: right;" class="text-success mt-3 font-weight-bold">'+charRemain+' caracteres restantes</span>';
-    }
+  if(charRemain == 0){
+    document.getElementById("contadorActividadDos").innerHTML = '<span id="contadorActividadDos" style="font-size: 12px; float: right;" class="text-danger mt-3 font-weight-bold">Haz llegado al límite de escritura</span>';
+  }else{
+    document.getElementById("contadorActividadDos").innerHTML = '<span id="contadorActividadDos" style="font-size: 12px; float: right;" class="text-success mt-3 font-weight-bold">'+charRemain+' caracteres restantes</span>';
+  }
 };
 
 $('#formActividad').submit(function(){
@@ -90,7 +90,7 @@ $('#formActividad').submit(function(){
         alertify.error(r);
       }
     });
-     return false;
+    return false;
   } else {
     $.ajax({
       type: 'POST',
@@ -107,7 +107,7 @@ $('#formActividad').submit(function(){
         alertify.error(r);
       }
     });
-     return false;
+    return false;
   }
 });
 
@@ -172,6 +172,27 @@ function resetDelete(){
 
 function acomodarEditarActividades(boton){
   var idActividad = boton.value;
+  var fila = boton.name;
+  var datos = [];
+
+  for (var i = 0; i < 12; i++) {
+    datos[i] = $("#EditarTablaModalActividad").children().children()[fila].children[i].innerHTML;
+  }
+
+  setTimeout(function (){
+    $('#selectinstitucion').focus();
+
+    $('#selectinstitucion').val(datos[10]);
+    $('#numeroAsistentes').val(datos[2]);
+    $('#numeroDespensas').val(datos[3]);
+    $('#Institucion1').val(datos[5]);
+    $('#fechaActividad').val(datos[11]);
+    $('#actividadUno').val(datos[8]);
+    $('#apoyoExtraCa').val(datos[4]);
+    $('#Institucion2').val(datos[6]);
+    $('#voluntariosCaritas').val(datos[7]);
+    $('#actividadDos').val(datos[9]);
+  }, 500);
 
   document.getElementById('deleteApoyame').outerHTML = '<button id="deleteApoyame" type="button" class="btn btn-danger" data-dismiss="modal" value = "'+idActividad+'">Cancelar</button>';
   document.getElementById('idActividadInput').innerHTML = '<input id="idActividadInput" value="" type="hidden" name="'+idActividad+'">';
