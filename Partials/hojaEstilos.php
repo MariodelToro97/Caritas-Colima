e13ca7ef2d1ed83c6fe7250cc9a8606e5488dfba
Mario Josue del Toro Morales <mario_chomy@outlook.com>
2019-01-27 12:48:59 -0600

    Hojas de estilos y Scripts locales
    Traslación de los scripts que estaban por CDN a recursos locales

--- Partials/hojaEstilos.php
+++ Partials/hojaEstilos.php
@@ -1,13 +1,11 @@
-<link rel="stylesheet" href="../assets/css/index_style.css">
+<link rel="stylesheet" href="../assets/css/index_style.css"/>
 <!-- CSS -->
-<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.11.2/build/css/alertify.min.css"/>
+<link rel="stylesheet" href="../assets/css/alertify.min.css"/>
 <!-- Default theme -->
-<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.11.2/build/css/themes/default.min.css"/>
+<link rel="stylesheet" href="../assets/css/default.min.css"/>
 <!-- Semantic UI theme -->
-<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.11.2/build/css/themes/semantic.min.css"/>
-<!-- Bootstrap theme -->
-<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.11.2/build/css/themes/bootstrap.min.css"/>
+<link rel="stylesheet" href="../assets/css/semantic.min.css"/>
 <!--Bootstrap-->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="../assets/css/bootstrap.min.css"/>
 <!--FontAwesome-->
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
--- Partials/hojaScript.php
+++ Partials/hojaScript.php
@@ -1,11 +1,10 @@
 <!--AJAX-->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="../assets/js/jquery.min.js" charset="utf-8"></script>
 <script src="../assets/js/Grupo.js" charset="utf-8"></script>
 <script src="../assets/js/Actividades.js" charset="utf-8"></script>
 <script src="../assets/js/Administrador.js" charset="utf-8"></script>
 <!-- JavaScript -->
-<script src="//cdn.jsdelivr.net/npm/alertifyjs@1.11.2/build/alertify.min.js"></script>
+<script src="../assets/js/alertify.min.js" charset="utf-8"></script>
 <!--Bootstrap-->
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"  crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="../assets/js/popper.min.js" charset="utf-8"></script>
+<script src="../assets/js/bootstrap.min.js" charset="utf-8"></script>
